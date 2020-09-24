319481984e09f3edc33423b506353c8af0595b4a
Harritone <yourrasik@gmail.com>
2020-09-24 11:38:12 +1000

    Implemented sending  new chats

--- liveChat/scripts/app.js
+++ liveChat/scripts/app.js
@@ -1,5 +1,16 @@
 // don queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+//adding a new chat
+newChatForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'shaun');
