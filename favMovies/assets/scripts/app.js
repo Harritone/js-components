d81aae5a715e0a8836beaf7903a1336eb30ff39b
Harritone <yourrasik@gmail.com>
2020-09-27 22:54:22 +1000

    Implemented creating movie object and clearing the input

--- favMovies/assets/scripts/app.js
+++ favMovies/assets/scripts/app.js
@@ -6,10 +6,19 @@ const backdrop = document.getElementById('backdrop');
 const confirmAddMovieBtn = cancelAddMovieBtn.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 
+const movies = [];
+
+const clearMovieInput = () => {
+  //for(const userInput of userInputs) {
+  //  userInput.value = '';
+  //}
+  userInputs.forEach(userInput => userInput.value = '');
+};
 
 const toggleModal = () => {
   addMovieModal.classList.toggle('visible');
   toggleBackdrop();
+  clearMovieInput();
   };
 
 const toggleBackdrop = () => {
@@ -24,7 +33,9 @@ const backdropClickHandler = () => {
   toggleModal();
 };
 
+
 const addMovieHandler = () => {
+
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
@@ -33,6 +44,14 @@ const addMovieHandler = () => {
     alert('Please enter valid values (rating between 1 and 5).');
     return;
   }
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  }
+  movies.push(newMovie);
+  console.log(movies);
+  toggleModal();
 };
 
 startAddMovieBtn.addEventListener('click', () => toggleModal());
