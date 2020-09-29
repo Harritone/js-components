7ec2aa62449c77b9d6a49431d41840511366990a
Harritone <yourrasik@gmail.com>
2020-09-29 19:47:26 +1000

    Implemented rendering elements

--- objects/assets/scripts/objects.js
+++ objects/assets/scripts/objects.js
@@ -3,10 +3,29 @@ const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
+
+
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  movies.forEach(movie => {
+    const movieEl = document.createElement('li');
+    movieEl.textContent = movie.info.title;
+    movieList.append(movieEl);
+  });
+}
+
 const addMovieHandler = () => {
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+  let title = document.getElementById('title').value;
+  let extraName = document.getElementById('extra-name').value;
+  let extraValue = document.getElementById('extra-value').value;
 
   if (title.trim === '' || extraName === '' || extraValue === '') {
     return;
@@ -19,6 +38,9 @@ const addMovieHandler = () => {
       id: Math.random()
     };
   movies.push(newMovie);
+  renderMovies();
 };
 
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+
