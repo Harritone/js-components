a9567055bbdc3d1737f1469acaa381c233e33d02
Harritone <yourrasik@gmail.com>
2020-09-30 21:54:17 +1000

    added inheritance and rearranged the order of execution

--- miniShop/assets/scripts/app.js
+++ miniShop/assets/scripts/app.js
@@ -16,9 +16,14 @@ class ElementAttribute {
 }
 
 class Component {
-  constructor(renderHookId) {
+  constructor(renderHookId, shouldRender = true) {
     this.hookId = renderHookId;
+    if(shouldRender) {
+    this.render();
+    }
   }
+
+  render() {};
 
   createRootElement(tag, cssClasses, attributes) {
     const rootElement = document.createElement(tag);
@@ -72,8 +77,9 @@ class ShopingCart extends Component {
 
 class ProductItem extends Component{
   constructor(product, renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
 
   addToCart() {
@@ -99,40 +105,53 @@ class ProductItem extends Component{
 }
 
 class ProductList extends Component {
-  products =  [
-    new Product(
-      'A Pillow',
-      'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-      'A soft pillow!',
-      19.99
-    ),
-    new Product(
-      'A Carpet',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-      'A carpet which you might like - or not.',
-      89.99
-    )
-  ];
+  products = [];
 
   constructor(renderHookId) {
     super(renderHookId);
-  };
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.products =  [
+      new Product(
+        'A Pillow',
+        'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+        'A soft pillow!',
+        19.99
+      ),
+      new Product(
+        'A Carpet',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+        'A carpet which you might like - or not.',
+        89.99
+      )
+    ];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.products) {
+      new ProductItem(prod,'prod-list');
+    }
+  }
 
   render() {
     this.createRootElement('ul', 'product-list', [new ElementAttribute('id', 'prod-list')]);
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod,'prod-list');
-      productItem.render();
+    if(this.products && this.products.length > 0) {
+      this.renderProducts();
     }
   }
 };
 
-class Shop {
+class Shop extends Component{
+  constructor() {
+    super();
+  }
+
   render() {
     this.cart = new ShopingCart('app');
-    this.cart.render()
-    const productList = new ProductList('app');
-    productList.render();
+    new ProductList('app');
   }
 }
 
@@ -141,7 +160,6 @@ class App {
 
   static initialize() {
     const shop = new Shop();
-    shop.render();
     this.cart = shop.cart;
   }
 
