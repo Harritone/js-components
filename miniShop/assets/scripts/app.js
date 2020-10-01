2d5558013be42bd6b1bd82260a51acb2b8f18619
Harritone <yourrasik@gmail.com>
2020-10-01 20:24:42 +1000

    Added Product, ProductItem and ProductList classes

---
+++ miniShop/assets/scripts/app.js
@@ -0,0 +1,61 @@
+class Product {
+  constructor(title, imgUrl, desc, price) {
+    this.title = title;
+    this.imgUrl = imgUrl;
+    this.desc = desc;
+    this.price = price;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imgUrl}" alt="${this.product.title}" />
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>${this.product.price}</h3>
+          <p>${this.product.desc}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product( 'A pillow',
+      'https://johnlewis.scene7.com/is/image/JohnLewis/236578658?$rsp-pdp-port-1080$', 
+      'A nice pillow', 24.99),
+    new Product( 'A carpet', 
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT2sLdkDkWMIxQhclWrVoXMKY_FsE9hpFYn4Q&usqp=CAU', 
+      'A carpet you will love', 99.99)
+  ]
+
+  constructor() {}
+
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      //const prodEl = new ProductItem(prod).render();
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    renderHook.append(prodList);
+  }
+}
+
+const prodList = new ProductList();
+prodList.render();
+
