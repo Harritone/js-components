b46dca69339f6393c7ebaa532b81a46adb496bc1
Harritone <yourrasik@gmail.com>
2020-10-05 16:19:29 +1000

    Added logic to switch projects

--- projectManager/assets/scripts/app.js
+++ projectManager/assets/scripts/app.js
@@ -3,18 +3,17 @@ class Tooltip {
 }
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFn) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFn;
     this.connectSwitchBtn();
     this.connectMoreInfoBtn();
   }
   connectSwitchBtn() {
     const switchBtn = document.getElementById(this.id).querySelector('button:last-of-type');
-    switchBtn.addEventListener('click', () => {
-      console.log('aaa');
-    })
+    switchBtn.addEventListener('click', this.updateProjectListsHandler);
   }
-  connectMoreInfoBtn() {
+    connectMoreInfoBtn() {
 
   }
 }
@@ -22,18 +21,22 @@ class ProjectItem {
 class ProjectList {
   projects = [];
   constructor(type){
-    //this.type = type;
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for(const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
     }
     console.log(this.projects);
   }
+  setSwitchHandlerFn(switchHandlerFn) {
+    this.switchHandler = switchHandlerFn;
+  }
   switchProject(projectId) {
+    this.switchHandler(this.projects.find(p => p === projectId));
     this.projects = this.projects.filter(p => p.id !== projectId);
   }
   addProject() {
-    
+    console.log(this);
   }
 }
 
@@ -41,6 +44,8 @@ class App {
   static initialize() {
     const activeProjectsList = new ProjectList('active');
     const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFn(finishedProjectsList.addProject.bind(finishedProjectsList));
+    finishedProjectsList.setSwitchHandlerFn(activeProjectsList.addProject.bind(activeProjectsList));
   }
 }
 
