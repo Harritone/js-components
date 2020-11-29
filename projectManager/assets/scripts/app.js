ad549ded7baec6365dd1e48be7bac560d8d2669a
Harritone <yourrasik@gmail.com>
2020-11-29 21:00:49 +1000

    Marked drope area

--- projectManager/assets/scripts/app.js
+++ projectManager/assets/scripts/app.js
@@ -149,6 +149,29 @@ class ProjectList {
         new ProjectItem(prjItem.id, this.switchProject.bind(this), type)
       );
     }
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+    list.addEventListener("dragenter", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+        list.parentElement.classList.add("droppable");
+      }
+    });
+    list.addEventListener("dragover", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener("dragleave", (event) => {
+      // console.log(event.relatedTarget.closest(`#${this.type}-projects ul`));
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
   }
 
   setSwitchHandlerFunction(switchHandlerFUnction) {
