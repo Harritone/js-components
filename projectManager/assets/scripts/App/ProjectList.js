a64a2b705eddc4019d33c962567e8077cee30898
Harritone <yourrasik@gmail.com>
2020-12-03 16:27:45 +1000

    Splitted code to modules

---
+++ projectManager/assets/scripts/App/ProjectList.js
@@ -0,0 +1,68 @@
+import { DOMHelper } from "../Utility/DOMHelper.js";
+import { ProjectItem } from "./ProjectItem.js";
+
+export class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    this.type = type;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), type)
+      );
+    }
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+    list.addEventListener("dragenter", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+        list.parentElement.classList.add("droppable");
+      }
+    });
+    list.addEventListener("dragover", (event) => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener("dragleave", (event) => {
+      // console.log(event.relatedTarget.closest(`#${this.type}-projects ul`));
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
+
+    list.addEventListener("drop", (event) => {
+      const prjId = event.dataTransfer.getData("text/plain");
+      if (this.projects.find((p) => p.id === prjId)) {
+        list.parentElement.classList.remove("droppable");
+        return;
+      }
+      document
+        .getElementById(prjId)
+        .querySelector("button:last-of-type")
+        .click();
+      list.parentElement.classList.remove("droppable");
+      // event.preventDefault(); // not required
+    });
+  }
+
+  setSwitchHandlerFunction(switchHandlerFUnction) {
+    this.switchHandler = switchHandlerFUnction;
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    project.update(this.switchProject.bind(this), this.type);
+  }
+
+  switchProject(projectId) {
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
