8704a19c37ad66539597c5247d668e7d7af41765
Harritone <yourrasik@gmail.com>
2020-12-03 17:57:13 +1000

    Named export syntax variations

--- projectManager/assets/scripts/App/ProjectList.js
+++ projectManager/assets/scripts/App/ProjectList.js
@@ -1,5 +1,5 @@
-import { DOMHelper } from "../Utility/DOMHelper.js";
-import { ProjectItem } from "./ProjectItem.js";
+import { ProjectItem as PrjItem } from "./ProjectItem.js";
+import * as DOMH from "../Utility/DOMHelper.js";
 
 export class ProjectList {
   projects = [];
@@ -9,7 +9,7 @@ export class ProjectList {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, this.switchProject.bind(this), type)
+        new PrjItem(prjItem.id, this.switchProject.bind(this), type)
       );
     }
     this.connectDroppable();
@@ -57,7 +57,7 @@ export class ProjectList {
 
   addProject(project) {
     this.projects.push(project);
-    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    DOMH.moveElement(project.id, `#${this.type}-projects ul`);
     project.update(this.switchProject.bind(this), this.type);
   }
 
