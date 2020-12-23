9c8958ec2758c32faf1ba671c4bcb2c25656b2ed
Harritone <yourrasik@gmail.com>
2020-12-23 19:22:24 +1000

    Implemented rolling the dice

--- pigGame/script.js
+++ pigGame/script.js
@@ -4,7 +4,35 @@
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// Variables
+let currentScore = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const rollDiceHandler = () => {
+  let score;
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  // diceEl.setAttribute('src', `dice-${dice}.png`);
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if true - switch to the next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO: change later
+  } else {
+    // Switch to next player
+  }
+};
+
+btnRoll.addEventListener('click', rollDiceHandler);
