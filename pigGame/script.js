6109a28224d6ccf9c9fa98457fb34076e8a93cad
Harritone <yourrasik@gmail.com>
2020-12-23 20:24:00 +1000

    Implemented switching player, hold and checking winner

--- pigGame/script.js
+++ pigGame/script.js
@@ -12,6 +12,8 @@ const current1El = document.getElementById('current--1');
 
 // Variables
 let currentScore = 0;
+let activePlayer = 0;
+const scores = [99, 99];
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -28,11 +30,60 @@ const rollDiceHandler = () => {
   // 3. Check for rolled 1: if true - switch to the next player
   if (dice !== 1) {
     // Add dice to current score
-    currentScore += dice;
-    current0El.textContent = currentScore; // TODO: change later
+    // currentScore += dice;
+    scores[activePlayer] += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    checkWinner();
   } else {
-    // Switch to next player
+    // reset current player's scores
+    // currentScore = 0;
+    scores[activePlayer] = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = '0';
+    // // remove active state from current player on UI
+    // document
+    //   .querySelector(`.player--${activePlayer}`)
+    //   .classList.remove('player--active');
+    // // switch player
+    // activePlayer = activePlayer === 0 ? 1 : 0;
+    // // add active state to current player on UI
+    // document
+    //   .querySelector(`.player--${activePlayer}`)
+    //   .classList.add('player--active');
+    switchPlayer();
   }
 };
 
+const checkWinner = function () {
+  if (scores[activePlayer] >= 100) {
+    // getCurrentPlayerEl(activePlayer).classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+  }
+};
+
+const getCurrentPlayerEl = function (activePlayer) {
+  return document.getElementById(`current--${activePlayer}`);
+};
+
+const switchPlayer = () => {
+  // remove active state from current player on UI
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--active');
+  // switch player
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  // add active state to current player on UI
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+};
+
+const holdHandler = () => {
+  switchPlayer();
+};
+
 btnRoll.addEventListener('click', rollDiceHandler);
+btnHold.addEventListener('click', holdHandler);
