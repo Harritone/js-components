b2bbfb605dbb7f9b697e1ff5fa88b2261e70c2ac
Hariton <yourrasik@gmail.com>
2021-05-08 14:50:12 +1000

    Add: smooth scrolling

--- bankist_web/script.js
+++ bankist_web/script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,4 +31,34 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+
+  // // window.scrollTo(
+  // //   s1coords.left + window.pageXOffset,
+  // //   s1coords.top + window.pageYOffset
+  // // );
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    if (this.getAttribute('href').length > 2) {
+      e.preventDefault();
+      document
+        .querySelector(this.getAttribute('href'))
+        .scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
