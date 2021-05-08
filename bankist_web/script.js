36ef1f75e2965a6e89ee687cfdc1fca6e62f4b75
Hariton <yourrasik@gmail.com>
2021-05-08 18:35:12 +1000

    Add: hover effect on nav

--- bankist_web/script.js
+++ bankist_web/script.js
@@ -8,6 +8,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContents = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,9 +38,6 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
 ///////////////////////////////////////
 // Smooth scrolling
@@ -62,17 +65,15 @@ btnScrollTo.addEventListener('click', function (e) {
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   if (e.target.classList.contains('nav__link')) {
     e.preventDefault();
-    document
-      .querySelector(e.target.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
+    if (e.target.getAttribute('href').length > 2) {
+      document
+        .querySelector(e.target.getAttribute('href'))
+        .scrollIntoView({ behavior: 'smooth' });
+    }
   }
 });
 
 // Tabbed Component
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContents = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', e => {
   const clicked = e.target.closest('.operations__tab');
@@ -88,3 +89,23 @@ tabsContainer.addEventListener('click', e => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+
+const handleHover = function (e) {
+  // console.log(this, e.target, e.currentTarget);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
