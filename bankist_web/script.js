c8ff0e79ace6227b58ea91b114c1c35614331200
Hariton <yourrasik@gmail.com>
2021-05-08 17:49:04 +1000

    Add: tabbed component

--- bankist_web/script.js
+++ bankist_web/script.js
@@ -67,3 +67,24 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
       .scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Tabbed Component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContents = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', e => {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContents.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
